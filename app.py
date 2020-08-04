1c4b29ee792f17a875d4fa894e7f35c3e467f3e5
akshajdevkv <akshajtechtips@gmail.com>
2020-08-04 12:12:00 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for, request ,redirect
+from markupsafe import escape
+import csv
+from emailsender import send_emails
+app = Flask(__name__)
+
+@app.route('/<string:page_name>')
+def web_url(page_name):
+	return render_template(page_name)
+@app.route('/')
+def redirect_to_home( ):
+	return redirect('/index.html')
+
+@app.route('/submit_data',methods=['POST'])
+def submit_data():
+	data = request.form.to_dict()
+	print(data)
+	if data['first-name'] == '' or data['last-name'] == '' or data['email'] == '':
+		return redirect('/tryagain.html')
+	else:
+		with open('database.csv','a') as csv_file:
+			csv_reader = csv.writer(csv_file)
+			csv_reader.writerow([data['first-name'],data['email']])
+
+	return redirect('/successful.html')
+
+# print(send_emails('akshajvaranacode404@gmail.com','danger197346'))
---
+++ emailsender.py
@@ -0,0 +1,45 @@
+import smtplib
+import csv
+import time
+from string import Template
+from email.message import EmailMessage
+
+email_address = 'akshajvaranacode404@gmail.com'
+email_password = 'danger197346'
+
+def send_emails(email_address,email_password):
+    while True:
+        with open('quotesource.txt','r') as f:
+            quotes = f.read()
+            daily_quote_num= 0
+            for quote in quotes.splitlines():
+                view_quote = quote
+                # opening user database
+                with open('database.csv', 'r') as csv_database:
+                    database_reader = csv.reader(csv_database)
+                    # looping through user database
+                    
+                    for database_info in database_reader:
+                        user_name = database_info[0]
+                        user_email = database_info[1]
+                        # sending email
+                        message = EmailMessage()
+                        message['from'] = user_name
+                        message['to'] = user_email
+                        message['subject'] = 'Daily Quotes '+str(daily_quote_num)
+                        t = Template('$quoteline\n \n Have a nice day $name !')
+                        message.set_content(t.substitute(
+                            quoteline= view_quote,name=user_name))
+                        try:
+                            with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                                smtp.ehlo()
+                                smtp.starttls()
+                                smtp.login(email_address, email_password)
+                                smtp.send_message(message)
+                                smtp.quit()
+                        except Exception as err:
+                            print(err)
+                        daily_quote_num += 1
+                time.sleep(86400)
+
+print(send_emails(email_address,email_password))
